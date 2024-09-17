d6e7f6989d0a3c98b6c7e4a225b2e929d9fad5a1
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-09-17 15:35:37 +0800

    update

--- src/components/domains/dashboard/bus-routes/BusSearchForm.tsx
+++ src/components/domains/dashboard/bus-routes/BusSearchForm.tsx
@@ -26,10 +26,10 @@ export const BusSearchForm: React.FC = () => {
         className="w-full h-[40px] text-xs border border-gray-200 rounded-lg px-3 mb-2"
         placeholder="TO"
       />
-      <View className="w-full border border-gray-200 rounded-lg relative p-1 mb-2">
+      {/* <View className="w-full border border-gray-200 rounded-lg relative p-1 mb-2">
         <Text className="text-xs text-gray-400 absolute top-3 left-3">SELECT DATE</Text>
         <RNDateTimePicker mode="date" value={date} onChange={handleChangeDate} />
-      </View>
+      </View> */}
 
       <Pressable className="w-full h-[40px] bg-red-700 rounded-lg justify-center items-center">
         <Text className="text-white font-medium">Search Bus</Text>
